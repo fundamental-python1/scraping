dce86425a6d526ba9e1b84e9790ca768464634f4
antenav <antenavmail@gmail.com>
2021-07-20 23:48:56 +0800

    done

---
+++ app.py
@@ -0,0 +1,12 @@
+import bs4
+import requests
+url = 'http://www.jadwalsholat.pkpu.or.id/?id=308'
+konten = requests.get(url)
+
+
+respon = bs4.BeautifulSoup(konten.text, "html.parser")
+cari_elemen = respon.find_all('tr', 'table_highlight')
+print(cari_elemen)
+
+sholat = {cari_elemen'td'}
+print(sholat)
